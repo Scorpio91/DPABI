92470a4048ad329b016493a90c91cc4da801bea6
Scorpio91 <psycho.ycs@gmail.com>
2019-03-19 01:43:37 +0800

    error calling parallel workers
    when load an autosaved .mat file without defining number of parallel workers, no Cfg.ParallelWorkersNumber would be found, and the DPARSFA_run.m would interrupt. 
    the problem bothers me when I would only like to use subfuntions like extracting time series or making functional connectivity based on preprocessed files from other toolboxes.

--- DPARSF/DPARSFA_run.m
+++ DPARSF/DPARSFA_run.m
@@ -1462,20 +1462,22 @@ end
 %     parpool(Cfg.ParallelWorkersNumber);
 % end
 % YAN Chao-Gan, 190312. To be compatible with early matlab versions
-PCTVer = ver('distcomp');
-if ~isempty(PCTVer)
-    FullMatlabVersion = sscanf(version,'%d.%d.%d.%d%s');
-    if FullMatlabVersion(1)*1000+FullMatlabVersion(2)<8*1000+3    %YAN Chao-Gan, 151117. If it's lower than MATLAB 2014a.  %FullMatlabVersion(1)*1000+FullMatlabVersion(2)>=7*1000+8    %YAN Chao-Gan, 120903. If it's higher than MATLAB 2008.
-        CurrentSize_MatlabPool = matlabpool('size');
-        if (CurrentSize_MatlabPool==0) && (Cfg.ParallelWorkersNumber~=0)
-            matlabpool(Cfg.ParallelWorkersNumber)
-        end
-    else
-        if isempty(gcp('nocreate')) && Cfg.ParallelWorkersNumber~=0
-            parpool(Cfg.ParallelWorkersNumber);
-        end
-    end
-end
+
+%scorpio91, 20190319. the calling of Cfg.ParallerWorkersNumber errored once load previously autosaved .mat files (in which no paraller workers were defined)
+% PCTVer = ver('distcomp');
+% if ~isempty(PCTVer)
+%    FullMatlabVersion = sscanf(version,'%d.%d.%d.%d%s');
+%    if FullMatlabVersion(1)*1000+FullMatlabVersion(2)<8*1000+3    %YAN Chao-Gan, 151117. If it's lower than MATLAB 2014a.  %FullMatlabVersion(1)*1000+FullMatlabVersion(2)>=7*1000+8    %YAN Chao-Gan, 120903. If it's higher than MATLAB 2008.
+%        CurrentSize_MatlabPool = matlabpool('size');
+%        if (CurrentSize_MatlabPool==0) && (Cfg.ParallelWorkersNumber~=0)
+%            matlabpool(Cfg.ParallelWorkersNumber)
+%        end
+%    else
+%        if isempty(gcp('nocreate')) && Cfg.ParallelWorkersNumber~=0
+%            parpool(Cfg.ParallelWorkersNumber);
+%        end
+%    end
+%end
 
 
 
